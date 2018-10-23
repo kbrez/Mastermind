6b8c34d1a701b6fcc080bbefa6d9de3ea09d7605
unknown <kylebrezovec@gmail.com>
2018-10-22 23:46:51 -0400

    First commit

---
+++ Mastermind/Mastermind.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Mastermind implementation by Kyle Brezovec for Quadax
+namespace Mastermind
+{
+    class Mastermind
+    {
+        static void Main(string[] args)
+        {
+            Boolean Playing = true;
+            Console.WriteLine("Welcome to mastermind. Can you guess the secret code?");
+            while(Playing)
+            {
+                Playing = Game();
+            }
+        }
+
+        static Boolean Game() //Main game function. We will stay here until the user exits
+        {
+            SecretCode Code = new SecretCode();
+            int RoundCounter = 10; //number of rounds determinded by requirements
+            do
+            {
+                Console.WriteLine(RoundCounter + " guesses remaining. Please enter your guess, a four digit code using integers 1-6");
+                String Input = Console.ReadLine();
+                int[] InputArray = ValidateAndConvertInput(ref Input);
+                List<char> Feedback = Code.CheckCode(InputArray);
+                Console.WriteLine("Your feedback:" );
+                Feedback.ForEach(Console.WriteLine);
+                if (IsVictorious(Feedback))
+                {
+                    break; //winner!
+                }
+                RoundCounter--;
+            } while (RoundCounter > 0);
+            if(RoundCounter == 0) //determine if we left the loop via win condition or from running out of tries
+            {
+                Console.WriteLine("You failed to guess the secrect code in 10 tunes. The code was " + String.Join("", new List<int>(Code.GetCode()).ConvertAll(i => i.ToString()).ToArray()));
+                return EndGame();
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You cracked the code " + String.Join("", new List<int>(Code.GetCode()).ConvertAll(i => i.ToString()).ToArray()));
+                return EndGame();
+            }
+        }
+
+        static int[] ValidateAndConvertInput(ref string Input) //all validation for the users guess
+        {
+            while (true)
+            {
+                if (int.TryParse(Input, out int n) & (n >= 1111 & n <= 6666) & !Input.Contains("0"))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter your guess, a four digit code using integers 1-6");
+                Input = Console.ReadLine();
+            }
+            char[] InputChar = Input.ToCharArray();
+            int[] InputInt = InputChar.Select(a => a - '0').ToArray();
+            return InputInt;
+        }
+
+        static Boolean EndGame() //return the users desire to play another round after validation
+        {
+            Console.WriteLine("Play Again? [Y/N]");
+            string PlayAgain = Console.ReadLine();
+            while (true)
+            {
+                if (string.Equals(PlayAgain, "Y", StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+                else if(string.Equals(PlayAgain, "N", StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+                Console.WriteLine("Invalid input. Play again? [Y/N]");
+                PlayAgain = Console.ReadLine();
+            }
+        }
+
+        static Boolean IsVictorious(List<char> Input)
+        {
+            List<char> VictoryList = new List<char>(new char[] { '+', '+', '+', '+' });
+            if (Input.SequenceEqual(VictoryList))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Mastermind/SecretCode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Secret code contains all functions needed for creating our code and checking if the users guess is correct
+namespace Mastermind
+{
+    class SecretCode
+    {
+        private readonly int[] Code = new int[4];
+
+        public SecretCode() //constructor creates new randomized code
+        {
+            CreateCode();
+        }
+
+        public List<char> CheckCode(int[] Guess) //cycle through the two lists, zeroing out any matches found
+        {
+            List<char> DecriptionFeedback = new List<char>();
+            int[] TempCode = new int[4];
+            Code.CopyTo(TempCode, 0);
+            for (int i = 0; i < Code.Length; i++) //first loop to check for exact matches
+            {
+                if (Guess[i] == Code[i])
+                {
+                    DecriptionFeedback.Insert(0, '+');
+                    Guess[i] = 0;
+                    TempCode[i] = 0;
+                }
+            }
+            for (int i = 0; i < Code.Length; i++) //second loop to check for correct digits out of place
+            {
+                if (TempCode.Contains(Guess[i]) & Guess[i] != 0)
+                {
+                    DecriptionFeedback.Add('-');
+                    TempCode[Array.IndexOf(TempCode, Guess[i])] = 0;
+                    Guess[i] = 0;
+                }
+            }
+            return DecriptionFeedback;
+        }
+
+        public void CreateCode()
+        {
+            Random rnd = new Random();
+            Code[0] = rnd.Next(1, 6);
+            Code[1] = rnd.Next(1, 6);
+            Code[2] = rnd.Next(1, 6);
+            Code[3] = rnd.Next(1, 6);
+        }
+
+        public int[] GetCode()
+        {
+            return Code;
+        }
+    }
+}
